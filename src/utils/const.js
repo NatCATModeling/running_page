0cf024d258132cc4f9f574dea9cb3fc44b1fea95
NatCATModeling <NatCATModeling@users.noreply.github.com>
2020-12-05 19:33:01 -0800

    Add files via upload

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 // const
-const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoibmF0Y2F0bW9kZWxpbmciLCJhIjoiY2lmcHBibmI5NmQ2dHMza3FmN3VhdnNveCJ9.cq75mgWIzoAaesbmdR6T6Q';
 const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
 
 // IF you outside China please make sure IS_CHINESE = false
